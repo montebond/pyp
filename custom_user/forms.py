f45e97d1ed4c4e8958c6a45052c542d480c31760
Michael Moncayo <michaelmoncayo@bond.local>
2017-10-06 22:38:59 -0500

    PK

--- custom_user/forms.py
+++ custom_user/forms.py
@@ -12,7 +12,7 @@ class EmailUserManager(BaseUserManager):
 
     """Custom manager for EmailUser."""
 
-    def _create_user(self, auto_id, email, password,
+    def _create_user(self, email, password,
                      is_superuser, is_staff, **extra_fields):
         """Create and save an EmailUser with the given email and password.
 
@@ -28,9 +28,8 @@ class EmailUserManager(BaseUserManager):
         if not email:
             raise ValueError('The given email must be set')
         email = self.normalize_email(email)
-        auto_id = models.AutoField("auto_id", primary_key=True)
         is_active = extra_fields.pop("is_active", True)
-        user = self.model(auto_id=auto_id, email=email, is_superuser=is_superuser, is_staff=is_staff,
+        user = self.model(email=email, is_superuser=is_superuser, is_staff=is_staff,
                           is_active=is_active, date_joined=now,
                           last_login=now, **extra_fields)
         user.set_password(password)
